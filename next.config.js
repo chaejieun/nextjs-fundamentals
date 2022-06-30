f34cc52bcf3c5b32423acead35126dea2e75afb3
jechae <jechae@creativehill.co.kr>
2022-06-30 16:23:00 +0900

    ADD : nomard coders first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { redirect } = require("next/dist/server/api-utils");
 
-module.exports = nextConfig
+const API_KEY = process.env.API_KEY;
+
+module.exports = {
+  reactStrictmode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
